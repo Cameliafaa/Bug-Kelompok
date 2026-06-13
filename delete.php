eec329401c090292d3bdb49b58dd2b039d0fc681
Intan <intanintan120405@gmail.com>
2026-06-13 15:26:27 +0700

    Fix Bug delete.php

--- delete.php
+++ delete.php
@@ -4,7 +4,8 @@ session_start();
 $id = $_GET['id'];
 
 foreach ($_SESSION['mahasiswa'] as $index => $mhs) {
-    // BUG 4: seharusnya == agar data yang dipilih saja yang dihapus
+
+    // BUG 4: operator salah, seharusnya ==
     if ($mhs['id'] != $id) {
         unset($_SESSION['mahasiswa'][$index]);
     }
